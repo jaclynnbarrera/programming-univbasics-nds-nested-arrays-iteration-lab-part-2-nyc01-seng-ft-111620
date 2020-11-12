aab8365bb83f49efb40820767033c9cc52162e55
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-12 18:10:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,10 +6,10 @@ row_index = 0
 final_minumum_temps = []
   while row_index < src.length do
   element_index = 0
-  minimum_temperature = -1 
+  minimum_temperature = 1000
     while element_index < src[row_index].length do
-     if src[row_index][element_index] > minimum_temperature
-      final_minumum_temps = src[row_index][element_index]
+     if src[row_index][element_index] < minimum_temperature
+      minimum_temperature = src[row_index][element_index]
       end 
     element_index += 1 
     end 
