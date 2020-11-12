83f7045bb3b02dfecc987ae3d04ab5f6af9b9f6a
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-12 17:58:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,9 +6,8 @@ row_index = 0
 final_minumum_temps = []
   while row_index < src.length do
   element_index = 0
-  minumum_element = 1000 
     while element_index < src[row_index].length do
-     if src[row_index][element_index].min
+     if src[row_index][element_index][0] ==  src[row_index][element_index].min
       final_minumum_temps = src[row_index][element_index]
       end 
     element_index += 1 
